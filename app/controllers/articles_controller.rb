ffa8ca96c747029f6bfd5cdc507ca0bc6d9a4819
zlotawanna <zlotawanna@gmail.com>
2020-11-25 20:25:55 +0100

    Creating articles messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,24 @@
 class ArticlesController <ApplicationController
 
   def show
-      @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def index
-      @articles = Article.all
+    @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 end
