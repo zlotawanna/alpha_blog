b1ec2a7292ad74be1a1f2b25b93ea38a93b2e706
zlotawanna <zlotawanna@gmail.com>
2021-05-28 16:08:43 +0200

    categories table creation, basic unit, funcional and integration tests added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,4 @@
-class ArticlesController <ApplicationController
+class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   before_action :require_user, except: [:show, :index]
   before_action :require_same_user, only: [:edit, :update, :destroy]
---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,31 @@
+class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category #{@category.name} was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def index
+
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
