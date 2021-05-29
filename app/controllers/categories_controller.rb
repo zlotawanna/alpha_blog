86475d583faf23abc82466b2928a59ce7d6cf217
zlotawanna <zlotawanna@gmail.com>
2021-05-29 19:47:41 +0200

    admin user rights for categories and tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def new
     @category = Category.new
@@ -15,7 +16,7 @@ class CategoriesController < ApplicationController
   end
 
   def index
-
+    @categories = Category.paginate(page: params[:page], per_page: 6)
   end
 
   def show
@@ -26,6 +27,13 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admin can perform this action"
+      redirect_to categories_path
+    end
   end
 
 end
